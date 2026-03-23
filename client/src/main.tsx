ffae704ac3e4f18c08ddc586602f41e205c8c92c
Ivan Solonenko <103060679+isolonenko@users.noreply.github.com>
2026-03-23 19:48:31 +0300

    refactor: remove MediaProvider wrapper from main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RoomProvider } from "./context/RoomContext";
-import { MediaProvider } from "./context/MediaProvider";
 import { App } from "./components/App";
 import "./styles/global.sass";
 
@@ -9,9 +8,7 @@ import "./styles/global.sass";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RoomProvider>
-      <MediaProvider>
-        <App />
-      </MediaProvider>
+      <App />
     </RoomProvider>
   </StrictMode>,
 );
